d6753048e6999254afd15dea06a9971754e60436
dapeng666111 <92576070+dapeng666111@users.noreply.github.com>
2021-11-29 09:59:16 +0800

    Add files via upload

--- main.c
+++ main.c
@@ -1,76 +1,32 @@
 #include <REGX52.H>
-//灯来回移动
+#include "delay.h"
 
-void Delay1ms(int t)		//@12.000MHz
-{
-	unsigned char i, j;
-	while(t)
-	{
-		i = 2;
-		j = 239;
-		do
-		{
-			while (--j);
-		} while (--i);
-			t--;
-	}
+
+void nixie(unsigned char led_locatin,num)
+{unsigned char a[]={0x3f,0x06,0x5b,0x4F,0x66,0x6D,0x7D,0x07,0x7F,0x6F};
+
+	//数码管位置
+	switch (led_locatin) 
+				{	case 7:P2_4=0;P2_3=0;P2_2=0;break;
+					case 6:P2_4=0;P2_3=0;P2_2=1;break;
+					case 5:P2_4=0;P2_3=1;P2_2=0;break;
+					case 4:P2_4=0;P2_3=1;P2_2=1;break;
+					case 3:P2_4=1;P2_3=0;P2_2=0;break;
+					case 2:P2_4=1;P2_3=0;P2_2=1;break;
+					case 1:P2_4=1;P2_3=1;P2_2=0;break;
+					case 0:P2_4=1;P2_3=1;P2_2=1;break;
+				}
+	//数字显示
+					P0=a[num];
+					delay(2);
+					P0=0x00;
+		
 }
 void main()
-{int led_num=0;
-	P2=0xfe;
+{
 	while(1)
-	{		
-		if (P3_1==0)
 		{
-			Delay1ms(20);
-			while(P3_1==0);
-			Delay1ms(20);
-			led_num++;
-			if (led_num>7)
-			{led_num=0;
-			} 
-			P2=~(0x01<<led_num);
-			
+			nixie(1,1);
+			nixie(2,2);
 		}
-		if (P3_0==0)
-		{
-			Delay1ms(20);
-			while(P3_0==0);
-			Delay1ms(20);
-			if (led_num==0)
-			{led_num=7;
-			}
-			else
-			{led_num--;
-			}
-			P2=~(0x01<<led_num);
-			
-		}
-	}		
-	
 }
-
-
-
-//#include <REGX52.H>
-//#include "delay.h"
-
-//控制灯的循环
-//void main ()
-//{		unsigned char num=0;
-//		P2=~(0x01);
-//	while(1)
-//	{	
-
-//		delay(5000);
-//		num++;
-//		if (num==7)
-//		{num=0;}
-//		P2=~(0x01<<num);
-
-//		
-//		
-//	}
-//}
-
-
